0f247c7ba67d9b12be90fffd8d56256481710231
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-02 22:17:14 +0300

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # trading-view-scraper
+
+## Setup
+
+mac:
+
+```bash
+run sudo /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
+```
--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const puppeteer = require("puppeteer");
 const functions = require("./functions");
+const credentials = require("./credentials");
 
-// sudo /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
 const ENDPIONT = "https://www.tradingview.com/chart/";
 
 (async () => {
-  const ws =
-    "ws://127.0.0.1:9222/devtools/browser/4a037f41-be9c-460a-9437-94c6c1a67dc0";
+  const ws = credentials.WS_URL;
   const browser = await puppeteer.connect({
     browserWSEndpoint: ws,
   });
