c0cd4249ade28eba0cba8af38079350d0d43e054
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-28 10:21:44 +0300

    fixes

--- runParams.js
+++ runParams.js
@@ -1,10 +1,17 @@
 const runParams = [
-  { LIST_NAME: "crypto", LAYOUT_NAME: "bot" },
-  { LIST_NAME: "funds", LAYOUT_NAME: "bot" },
-  { LIST_NAME: "futures", LAYOUT_NAME: "bot" },
-  { LIST_NAME: "fxcm", LAYOUT_NAME: "bot" },
-  { LIST_NAME: "indices", LAYOUT_NAME: "bot" },
-  { LIST_NAME: "stocks", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto1", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto2", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto3", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto4", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto5", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "crypto6", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "funds", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "futures", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "fxcm", LAYOUT_NAME: "bot" },
+  { LIST_NAME: "capital", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "indices", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "stocks", LAYOUT_NAME: "bot" },
 ];
 
 module.exports = {
