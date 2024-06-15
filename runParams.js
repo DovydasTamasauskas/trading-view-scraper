27eb6eed20e2df086664c2e0b45bcd7f0f30e1c7
DovydasT <tamasauskas.dovydas@gmail.com>
2024-06-15 11:16:38 +0300

    param fix

--- runParams.js
+++ runParams.js
@@ -1,15 +1,8 @@
 const runParams = [
   // { LIST_NAME: "crypto", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "crypto1", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "crypto2", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "crypto3", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "crypto4", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "crypto5", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "crypto6", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "funds", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "futures", LAYOUT_NAME: "bot" },
-  // { LIST_NAME: "fxcm", LAYOUT_NAME: "bot" },
-  { LIST_NAME: "capital", LAYOUT_NAME: "bot" },
+  { LIST_NAME: "forex", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "indices", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "stocks", LAYOUT_NAME: "bot" },
 ];
