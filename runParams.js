d6ef5c539a05e7fa0b207e492b9434b6f5518f7e
DovydasT <tamasauskas.dovydas@gmail.com>
2024-08-22 08:03:52 +0300

    added start script

--- runParams.js
+++ runParams.js
@@ -1,5 +1,6 @@
 const runParams = [
   // { LIST_NAME: "crypto", LAYOUT_NAME: "bot" },
+  // { LIST_NAME: "mix", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "funds", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "futures", LAYOUT_NAME: "bot" },
   // { LIST_NAME: "forex", LAYOUT_NAME: "bot" },
